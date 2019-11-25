010ffe113adc048c13fac838a37bcd74730ab151
Lecuyer Stéphane <47177118+lecuyste@users.noreply.github.com>
2019-11-25 16:05:42 +0100

    Ajout et modification de renseignement ok mis a part le radio qui ne revient pas si modification

--- include/renseignement.inc.php
+++ include/renseignement.inc.php
@@ -1,14 +1,13 @@
 <h1>Renseignements</h1>
 <?php
-if (isset($_POST['rens'])) {
+if (isset($_POST['submitted'])) {
     $birthDate = isset($_POST['birthDate']) ? $_POST['birthDate'] : "";
     $birthLoc = isset($_POST['birthLoc']) ? $_POST['birthLoc'] : "";
     $poids = isset($_POST['poids']) ? $_POST['poids'] : "";
     $taille = isset($_POST['taille']) ? $_POST['taille'] : "";
     $sang = isset($_POST['sang']) ? $_POST['sang'] : "";
-    $sql = "SELECT mail FROM users";
-    $retourMail = $pdo->query($sql);
-    $mail = $retourMail->fetch();
+    $id_user = $_SESSION['login']['id'];
+
     $erreurs = array();
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $birthDate = test_input($_POST["birthDate"]);
@@ -16,6 +15,7 @@ if (isset($_POST['rens'])) {
         $poids = test_input($_POST["poids"]);
         $taille = test_input($_POST["taille"]);
         $sang = test_input($_POST["sang"]);
+
     }
     if (!(mb_strlen($birthLoc) >= 2 && ctype_alpha($birthLoc)))
         array_push($erreurs, "Veuillez saisir votre lieu de naissance.");
@@ -30,24 +30,14 @@ if (isset($_POST['rens'])) {
         echo $message;
         include "formRenseignement.php";
     } else {
-        $sql = "SELECT COUNT(*) FROM users WHERE mail='". $mail["mail"] . "'";
-        $nombreOccurences = $pdo->query($sql)->fetchColumn();
-        if ($nombreOccurences <= 5) {
-            $sql = "INSERT INTO users
-                (birthDate, birthLoc, poids, taille, sang)
-                VALUES ('" . $birthDate . "', '" . $birthLoc . "', '" . $poids . "', '" . $taille . "', '" . $sang . "')";
+            $sql = "UPDATE users SET birthDate = :birthDate, birthLoc = :birthLoc, poids = :poids, taille = :taille, sang = :sang, modifiedAt = NOW() WHERE idUser = $id_user";
             $query = $pdo->prepare($sql);
-            $query->bindValue('birthDate', $birthDate, PDO::PARAM_STR);
-            $query->bindValue('birthLoc', $birthLoc, PDO::PARAM_STR);
-            $query->bindValue('poids', $poids, PDO::PARAM_STR);
-            $query->bindValue('taille', $taille, PDO::PARAM_STR);
-            $query->bindValue('sang', $sang, PDO::PARAM_STR);
+            $query->bindValue(':birthDate', $birthDate, PDO::PARAM_STR);
+            $query->bindValue(':birthLoc', $birthLoc, PDO::PARAM_STR);
+            $query->bindValue(':poids', $poids, PDO::PARAM_INT);
+            $query->bindValue(':taille', $taille, PDO::PARAM_INT);
+            $query->bindValue(':sang', $sang, PDO::PARAM_STR);
             $query->execute();
-        }
-        else {
-            echo $mail["mail"] . "</br>";
-            echo "Vous avez déjà rempli des informations";
-        }
     }
 } else {
     require_once "formRenseignement.php";

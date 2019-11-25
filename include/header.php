40bfebedeb1cc8bcc78429cf2e31dccdc9208851
Lecuyer Stéphane <47177118+lecuyste@users.noreply.github.com>
2019-11-25 16:03:59 +0100

    Modification header si admin ou non

--- include/header.php
+++ include/header.php
@@ -1,10 +1,34 @@
 <body>
 <header>
                 <?php
-                if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
-                    echo "<li><a href=\"index.php?page=renseignement\">Mes Renseignements</a></li>";
+                if (isAdmin()) {
+                    $id_user = $_SESSION['login']['id'];
+                    $sql = "SELECT * FROM users where idUSER = $id_user";
+                    $query = $pdo->prepare($sql);
+                    $query->execute();
+                    $profil = $query->fetch();
+                    if (empty($profil['modifiedAt'])){
+                        echo "<li><a href=\"index.php?page=renseignement\">Ajouter mes Renseignements</a></li>";
+                    } else {
+                        echo "<li><a href=\"index.php?page=renseignement\">Modifier mes Renseignements</a></li>";
+                    }
                     echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
-                } else {
+                    echo "<h1> Vous êtes Admin ! </h1>";
+                } elseif (isLoged()){
+                    $id_user = $_SESSION['login']['id'];
+                    $sql = "SELECT * FROM users where idUSER = $id_user";
+                    $query = $pdo->prepare($sql);
+                    $query->execute();
+                    $profil = $query->fetch();
+                    if (empty($profil['modifiedAt'])){
+                        echo "<li><a href=\"index.php?page=renseignement\">Ajouter mes Renseignements</a></li>";
+                    } else {
+                        echo "<li><a href=\"index.php?page=renseignement\">Modifier mes Renseignements</a></li>";
+                    }
+                    echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+                    echo "<h1> Vous êtes utilisateur ! </h1>";
+                }
+                else {
                     echo "<li><a href=\"index.php?page=inscription\">Inscription</a></li>";
                     echo "<li><a href=\"index.php?page=login\">Connexion</a></li>";
                     if (isset($_POST['inscr'])) {

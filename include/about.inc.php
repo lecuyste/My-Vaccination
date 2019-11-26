cfac24e1d951e2f137071fdf4e22b218047b2fcc
Lecuyer Stéphane <47177118+lecuyste@users.noreply.github.com>
2019-11-26 11:56:09 +0100

    Affiche profil si logged

--- include/about.inc.php
+++ include/about.inc.php
@@ -1,1 +1,52 @@
 <h2> Blablabla </h2>
+<?php
+
+if (isLoged()){
+    $id_user = $_SESSION['login']['id'];
+    $sql = "SELECT * FROM users where idUSER = $id_user";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $profil = $query->fetch();
+    echo "<h2> Mon profil </h2>";
+    ?>
+<p> Mon nom : <?php if (!empty($profil['nom'])){
+    echo $profil['nom'];
+} else {
+    echo "Champ non renseigné";
+}?></p>
+    <p> Mon prénom : <?php if (!empty($profil['prenom'])){
+            echo $profil['prenom'];
+        } else {
+            echo "Champ non renseigné";
+        }?></p>
+    <p> Mon adresse mail : <?php if (!empty($profil['mail'])){
+            echo $profil['mail'];
+        } else {
+            echo "Champ non renseigné";
+        }?></p>
+    <p> Ma date de naissance : <?php if (!empty($profil['birthDate'])){
+            echo strftime("%d %B %G", strtotime($profil['birthDate']));
+        } else {
+            echo "Champ non renseigné";
+        }?></p>
+    <p> Mon lieu de naissance : <?php if (!empty($profil['birthLoc'])){
+            echo $profil['birthLoc'];
+        } else {
+            echo "Champ non renseigné";
+        }?></p>
+    <p> Mon poids de naissance : <?php if (!empty($profil['poids'])){
+            echo $profil['poids'] . " grammes";
+        } else {
+            echo "Champ non renseigné";
+        }?></p>
+    <p> Ma taille de naissance : <?php if (!empty($profil['taille'])){
+            echo $profil['taille'] . " centimètres";
+        } else {
+            echo "Champ non renseigné";
+        }?></p>
+    <p> Mon groupe sanguin : <?php if (!empty($profil['sang'])){
+            echo $profil['sang'];
+        } else {
+            echo "Champ non renseigné";
+        }?></p>
+<?php }

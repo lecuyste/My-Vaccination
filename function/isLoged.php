aefb7541dcd4e586dd66bdcdadcdd51c8d3b8d0e
Lecuyer Stéphane <47177118+lecuyste@users.noreply.github.com>
2019-11-28 11:37:20 +0100

    Ajout du compte Admin avec securisation de la page

--- function/isLoged.php
+++ function/isLoged.php
@@ -2,7 +2,6 @@
 function isLoged()
 {
     if (!empty ($_SESSION['login'])) {
-
         if (!empty($_SESSION['login']['id']) && filter_var($_SESSION['login']['id'], FILTER_VALIDATE_INT)) {
             if (!empty($_SESSION['login']['email']) && filter_var($_SESSION['login']['email'], FILTER_VALIDATE_EMAIL)) {
                 if (!empty($_SESSION['login']['role']) && filter_var($_SESSION['login']['role'], FILTER_VALIDATE_INT)) {
